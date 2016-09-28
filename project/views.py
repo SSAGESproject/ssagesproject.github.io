410a23e43f80e94b5c026e52aaf3a2a16e6b99e9
cbezik <harthan@gmail.com>
2016-09-28 16:01:49 -0500

    Fixing

--- project/views.py
+++ project/views.py
@@ -4,8 +4,10 @@ from app import app, pages
 #------------------ general page routes ----------------------#
 @app.route('/')
 @app.route('/index')
-@app.route('/home')
 def index():
+    return render_template('index.html')
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 @app.route('/features')
